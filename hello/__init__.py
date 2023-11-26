03e23fb0e023e72026af56220bdde46e4b37b67f
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-11-27 02:56:22 +0400

    update hello test

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,6 +1,22 @@
-# import check50
+import check50
 
-# @check50.check()
-# def hello_world():
-#     """hello world"""
-#     check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
+
+@check50.check()
+def exists():
+    """sayhello.py exists"""
+    check50.exists("sayhello.py")
+
+@check50.check(exists)
+def testoto():
+    """input of Oto yields output of 'Hello, Oto, nice to meet you!'"""
+    check50.run("python3 sayhello.py").stdin("Oto", prompt=True).stdout("Hello, Oto, nice to meet you!").exit()
+
+@check50.check(exists)
+def testgiorgi():
+    """input of Giorgi yields output of 'Hello, Giorgi, nice to meet you!'"""
+    check50.run("python3 sayhello.py").stdin("Giorgi", prompt=True).stdout("Hello, Giorgi, nice to meet you!").exit()
+
+@check50.check(exists)
+def testbitcamp():
+    """input of Bitcamp yields output of 'Hello, Bitcamp, nice to meet you!'"""
+    check50.run("python3 sayhello.py").stdin("Bitcamp", prompt=True).stdout("Hello, Bitcamp, nice to meet you!").exit()
